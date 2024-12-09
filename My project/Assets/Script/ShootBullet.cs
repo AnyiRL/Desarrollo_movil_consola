bd531430839905ca9c2097e31589ee2cd568118f
AnyiRL <ruananyi7@gmail.com>
2024-12-09 13:42:22 +0100

    Pool

---
+++ My project/Assets/Script/ShootBullet.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootBullet : MonoBehaviour
+{
+    public GameObjectPool bulletPool;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1")) 
+        {
+            GameObject obj = bulletPool.GimmeInactiveGameObject();
+
+            if (obj)
+            {               
+                obj.SetActive(true); // ya no esta disponible en la pool
+                obj.transform.position = transform.position;
+                obj.GetComponent<Bullet>().SetDirection(transform.forward);
+            }
+        }      
+    }
+}
